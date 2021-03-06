6546cc204ad6e15ca2528df61eb0c769bb9932b0
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-03-06 22:41:16 +0900

    Create a REPL skelton

---
+++ repl.ml
@@ -0,0 +1,24 @@
+open Base
+open Stdio
+
+let read x = x
+
+let eval x = x
+
+let print x = x
+
+let _ =
+  let open In_channel in
+  let open Out_channel in
+  let rec loop () =
+    printf "(olisp)> %!"; (* %! for flush before readline *)
+    input_line_exn stdin
+    |> read
+    |> eval
+    |> print
+    |> print_endline;
+    loop ()
+  in
+  try
+    loop ()
+  with End_of_file -> Caml.exit 0
