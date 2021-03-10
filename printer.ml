9e604233ae122b2d2dedc66946622a53644f024d
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-03-10 19:55:56 +0900

    Print nil as empty string

--- printer.ml
+++ printer.ml
@@ -5,7 +5,7 @@ let rec print_str = function
   | Ast.Symbol x -> x
   | Ast.Int x -> Int.to_string x
   | Ast.Bool x -> if x then "true" else "false"
-  | Ast.Nil -> "nil"
+  | Ast.Nil -> ""
   | Ast.Fn _ -> "<function>"
   | Ast.List lst ->
       List.map lst ~f:(fun x -> print_str x)
