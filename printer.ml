1c4e1991a2e44dbb21c45d8ddbc32ee9947860d9
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-03-11 19:30:11 +0900

    Printer accepts readably flag

--- printer.ml
+++ printer.ml
@@ -1,9 +1,14 @@
 open Base
 open Printf
 
-let rec print_str = function
+let escape s =
+  String.substr_replace_all ~pattern:{|\|} ~with_:{|\\|} s
+  |> String.substr_replace_all ~pattern:"\n" ~with_:{|\n|}
+  |> String.substr_replace_all ~pattern:{|"|} ~with_:{|\"|}
+
+let rec print_str ?(readably=true) = function
   | Ast.Symbol x -> x
-  | Ast.String x -> x
+  | Ast.String x -> if readably then x else escape x
   | Ast.Int x -> Int.to_string x
   | Ast.Bool x -> if x then "true" else "false"
   | Ast.Nil -> ""
