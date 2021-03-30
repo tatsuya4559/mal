d05807823b968e63acb6369e3701e1044a6ab2e0
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-03-30 19:26:20 +0900

    Rewrite Assoc -> Map

--- lib/env.ml
+++ lib/env.ml
@@ -1,29 +1,28 @@
 open Core
 
-module Assoc = List.Assoc
-
-(* TODO: re-implement using Map. I don't understand functor yet... *)
 type t = {
-  mutable store: (string, Ast.t) Assoc.t;
+  mutable store : (string, Ast.t, String.comparator_witness) Map.t;
   outer: t option;
 }
 
 let rec get t key =
   let open Option.Monad_infix in
-  match Assoc.find ~equal:String.equal t.store key with
+  match Map.find t.store key with
   | Some value -> Some value
   | None -> t.outer >>= fun outer -> get outer key
 
-
 let set t key value =
-  t.store <- Assoc.add ~equal:String.equal t.store key value
+  let store = Map.update t.store key ~f:(fun _ -> value) in
+  t.store <- store
 
 let make ?(binds=[]) () =
-  let env = { store = []; outer = None } in
+  let store = Map.empty (module String) in
+  let env = { store; outer = None } in
   List.iter binds ~f:(fun (key, value) -> set env key value);
   env
 
 let enclose ?(binds=[]) t =
-  let env = { store = []; outer = Some t } in
+  let store = Map.empty (module String) in
+  let env = { store; outer = Some t } in
   List.iter binds ~f:(fun (key, value) -> set env key value);
   env
